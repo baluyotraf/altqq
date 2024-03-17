4b9081b9bd22347091963d9601dccfdb9e4841c6
baluyotraf <baluyotraf@outlook.com>
2024-03-17 20:06:45 +0100

    Improve plain text translator to handle data types

--- src/altqq/translators/plain_text.py
+++ src/altqq/translators/plain_text.py
@@ -1,8 +1,12 @@
+"""Module for converting Query objects to plain text SQL"""
+
 import dataclasses as dc
+import typing
 from typing import Any
 
 from altqq.structs import Query
-from altqq.types import T
+from altqq.types import QueryValueTypes, T
+from typing_extensions import Annotated
 
 
 class PlainTextTranslator:
@@ -10,7 +14,22 @@ class PlainTextTranslator:
         value = getattr(query, field.name)
         if field.type == Query:
             return self.__call__(value)
-        return value
+
+        if typing.get_origin(field.type) == Annotated:
+            # Pyright can't match the __metadata__ attribute to Annotated
+            if QueryValueTypes.NON_PARAMETER in field.type.__metadata__:  # type: ignore
+                return value
+
+        # Numeric types are not escaped
+        if isinstance(value, (int, float)):
+            return value
+
+        # None is written as NULL
+        if value is None:
+            return "NULL"
+
+        # All other types fall down to strings and are escaped
+        return f"'{value}'"
 
     def __call__(self, query: Query) -> str:
         fields = dc.fields(query)
