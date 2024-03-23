37594f4e788160e998ffe1212ad0db748972c8e0
baluyotraf <baluyotraf@outlook.com>
2024-03-23 01:18:19 +0100

    Add initial implementation of psycopg translator

--- src/altqq/__init__.py
+++ src/altqq/__init__.py
@@ -3,6 +3,7 @@
 from altqq.structs import Calculated as Calculated
 from altqq.structs import Query
 from altqq.translators.plain_text import PlainTextTranslator
+from altqq.translators.psycopg import PsycopgQuery, PsycopgTranslator
 from altqq.translators.pyodbc import PyODBCQuery, PyODBCTranslator
 from altqq.types import NonParameter as NonParameter
 
@@ -11,6 +12,7 @@ class Translators:
     """Definition of available translators."""
 
     PYODBC = PyODBCTranslator()
+    PSYCOPG = PsycopgTranslator()
     PLAIN_TEXT = PlainTextTranslator()
 
 
@@ -24,6 +26,18 @@ def to_pyodbc(query: Query) -> PyODBCQuery:
         PyODBCQuery: Equivalent query for PyODBC usage.
     """
     return Translators.PYODBC(query)
+
+
+def to_psycopg(query: Query) -> PsycopgQuery:
+    """Converts a `Query` to its corresponding `PsycopgQuery` object.
+
+    Args:
+        query (Query): Query to translate to Psycopg
+
+    Returns:
+        PsycopgQuery: Equivalent query for Psycopg usage.
+    """
+    return Translators.PSYCOPG(query)
 
 
 def to_plain_text(query: Query) -> str:
